5ffdc14848ccaae62b0b36c6d483195c14faf78f
Philip Shamsiev <ph.shamsiev@mail.ru>
2016-04-05 17:19:32 +0300

    Final check

--- js/menu.js
+++ js/menu.js
@@ -5,23 +5,15 @@
   var iconCross = document.querySelector('.cross');
   var pageLogo = document.querySelector('.page-logo');
   isMenuOpen = false;
-  
+
   mobileMenuOpen.addEventListener('tap', function(event) {
     event.preventDefault();
     if (isMenuOpen == false) {
-      pageLogo.style.borderBottom = '1px solid #000000';
-      pageLogo.style.background = '#283645';
-      nav.style.display = 'block';
-      iconBurger.style.display = 'none';
-      iconCross.style.display = 'block';
+      pageLogo.classList.add('page-logo--menu-show');
       isMenuOpen = true;
     }
     else if (isMenuOpen == true) {
-      pageLogo.style.borderBottom = 'none';
-      pageLogo.style.background = 'rgba(0, 0, 0, .3)';
-      nav.style.display = 'none';
-      iconBurger.style.display = 'block';
-      iconCross.style.display = 'none';
+      pageLogo.classList.remove('page-logo--menu-show');
       isMenuOpen = false;
     }
   });
